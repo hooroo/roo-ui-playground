9dab116ddcad356aea35722f364d92fe7d92edbc
Angus Fretwell <angusfretwell@me.com>
2019-08-22 11:13:47 +1000

    setup latest roo-ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { qantasHotels } from 'roo-ui/logos';
+import {
+  Container,
+  Box,
+  Heading,
+  Paragraph,
+  Button,
+  Image,
+} from 'roo-ui/components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+const App = () => (
+  <Fragment>
+    <Box bg="greys.porcelain" textAlign="center">
+      <Container py={12}>
+        <Image src={qantasHotels} inline mb={6} />
+        <Heading.h1>Hello, Roo UI!</Heading.h1>
+      </Container>
+    </Box>
+
+    <Box textAlign="center">
+      <Container py={12}>
+        <Paragraph fontSize="lg" mb={6}>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        </Paragraph>
+
+        <Button
+          as="a"
+          href="https://github.com/hooroo/roo-ui/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          Roo UI on Github
+        </Button>
+      </Container>
+    </Box>
+  </Fragment>
+);
 
 export default App;
