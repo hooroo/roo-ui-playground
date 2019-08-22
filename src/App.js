999ab9c3a41e1c6b39a580c34d37afb07812f231
Angus Fretwell <angusfretwell@me.com>
2019-08-22 11:05:47 +1000

    upgrade to latest create-react-app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-import React, { Fragment } from "react";
-import { qantasHotels } from "@roo-ui/logos";
-import {
-  Container,
-  Box,
-  Heading,
-  Paragraph,
-  Button,
-  Image
-} from "@roo-ui/components";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => (
-  <Fragment>
-    <Box bg="grey.3" textAlign="center">
-      <Container py={12}>
-        <Image src={qantasHotels} inline mb={6} />
-        <Heading.h1>Hello, Roo UI!</Heading.h1>
-      </Container>
-    </Box>
-
-    <Box textAlign="center">
-      <Container py={12}>
-        <Paragraph fontSize="lg" mb={6}>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
           Edit <code>src/App.js</code> and save to reload.
-        </Paragraph>
-
-        <Button
-          as="a"
-          href="https://github.com/hooroo/roo-ui/"
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Roo UI on Github
-        </Button>
-      </Container>
-    </Box>
-  </Fragment>
-);
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import 'normalize.css/normalize.css';
-import '@roo-ui/fonts/ciutadella.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import { ThemeProvider, qantas } from '@roo-ui/themes';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <ThemeProvider theme={qantas}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
